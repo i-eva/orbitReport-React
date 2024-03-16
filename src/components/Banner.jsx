474053b5889403fc0a734b0ecde808015e66cc75
i-eva <ievaswanson@u.boisestate.edu>
2024-03-16 10:28:30 -0500

    Task 1 completed. Task 2 completed.

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+export default function Banner() {
+    return(
+        <div>
+            <header>Orbit Report</header>
+            <p>Select an orbit button to see satellites</p>
+        </div>
+    );
+}
